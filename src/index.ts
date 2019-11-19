005750b75e8b9ffe3765500a71c22b09d34e35de
Thomas George <Jorge0136@gmail.com>
2019-11-19 00:00:56 -0800

    adds inquirer method to select context

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import Command from './base-command'
 import {flags} from '@oclif/command'
-import cli from 'cli-ux';
 import * as _ from 'lodash'
+import * as inquirer from 'inquirer';
 
 class OclifConfigIncluded extends Command {
   static description = 'describe the command here'
 
   static flags = {
-    // Boilerplate to used extended command
+    // Boilerplate to use extended command
     ...Command.flags,
     help: flags.help({char: 'h'}),
     context: flags.string({
       char: 'c',
-      description: 'Current config to use'
+      description: 'Current config to use. Example: local'
     })
   }
 
@@ -22,24 +22,23 @@ class OclifConfigIncluded extends Command {
     let userSelectedContext: string
     
     if (!flags.context)  {
-      this.log('Your current contexts are:\n')
-      _.forEach(this.allConfig.contexts, (config) => { console.log(config.name) });
-      userSelectedContext = await cli.prompt('Which context do you wish to use?')
+      const userSelection = await inquirer.prompt([{ 
+        name: 'currentContext',
+        type: 'list',
+        message: 'Select a context:',
+        choices: _.map(this.allConfig.contexts, 'name')
+      }]);
+      userSelectedContext = userSelection.currentContext;
     }
-
+    
     else userSelectedContext = flags.context;
-
-    console.log(userSelectedContext);
 
     // Unfortunate need to merge the top level config and the current selection
     // as we can't just update one property of the config as it's currently designed. 
     // I think this is a consequence of using the `set`ter. 
     this.allConfig = {...this.allConfig, ...{currentContext: userSelectedContext}};
-    this.log(JSON.stringify(this.allConfig))
     this.log(JSON.stringify(this.activeConfig));
   }
 }
 
 export = OclifConfigIncluded
-
-
